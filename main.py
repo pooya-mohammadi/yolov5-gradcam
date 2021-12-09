704f38c99be4a4415d1763b4bfab240f049c67ce
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-12-10 00:29:08 +0330

    Initial Commits

---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import time
+import argparse
+import numpy as np
+from models.gradcam import YOLOV5GradCAM
+from models.yolo_v5_object_detector import YOLOV5TorchObjectDetector
+import cv2
+from deep_utils import show_destroy_cv2, Box, split_extension
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-path', type=str, default="yolov5s.pt", help='Path to the model')
+parser.add_argument('--img-path', type=str, default='images/eagle.jpg', help='input image path')
+parser.add_argument('--img-size', type=int, default=640, help="input image size")
+parser.add_argument('--target-layer', type=str, default='model_23_cv3_act',
+                    help='The layer hierarchical address to which gradcam will applied,'
+                         ' the names should be separated by underline')
+parser.add_argument('--method', type=str, default='gradcam', help='gradcam or gradcampp')
+parser.add_argument('--device', type=str, default='cpu', help='cuda or cpu')
+
+args = parser.parse_args()
+
+
+def get_res_img(bbox, mask, res_img):
+    mask = mask.squeeze(0).mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).detach().cpu().numpy().astype(
+        np.uint8)
+    heatmap = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+    n_heatmat = (Box.fill_outer_box(heatmap, bbox) / 255).astype(np.float32)
+    res_img = res_img / 255
+    res_img = cv2.add(res_img, n_heatmat)
+    res_img = (res_img / res_img.max())
+    return res_img, n_heatmat
+
+
+def put_text_box(bbox, cls_name, res_img):
+    x1, y1, x2, y2 = bbox
+    # this is a bug in cv2. It does not put box on a converted image from torch unless it's buffered and read again!
+    cv2.imwrite('temp.jpg', (res_img * 255).astype(np.uint8))
+    res_img = cv2.imread('temp.jpg')
+    res_img = Box.put_box(res_img, bbox)
+    res_img = Box.put_text(res_img, cls_name, (x1, y1))
+    return res_img
+
+
+def concat_images(images):
+    w, h = images[0].shape[:2]
+    width = w
+    height = h * len(images)
+    base_img = np.zeros((width, height, 3), dtype=np.uint8)
+    for i, img in enumerate(images):
+        base_img[:, h * i:h * (i + 1), ...] = img
+    return base_img
+
+
+def main():
+    global device, model
+    device = args.device
+    input_size = (args.img_size, args.img_size)
+    img = cv2.imread(args.img_path)
+    print('[INFO] Loading the model')
+    model = YOLOV5TorchObjectDetector(args.model_path, 'cpu', img_size=input_size)
+    torch_img = model.preprocessing(img[..., ::-1])
+    if args.method == 'gradcam':
+        saliency_method = YOLOV5GradCAM(model=model, layer_name=args.target_layer, img_size=input_size)
+    tic = time.time()
+    masks, logits, [boxes, _, class_names, _] = saliency_method(torch_img)
+    print("total time:", round(time.time() - tic, 4))
+    result = torch_img.squeeze(0).mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).detach().cpu().numpy()
+    result = result[..., ::-1]  # convert to bgr
+    images = [result]
+    for i, mask in enumerate(masks):
+        res_img = result.copy()
+        bbox, cls_name = boxes[0][i], class_names[0][i]
+        res_img, heat_map = get_res_img(bbox, mask, res_img)
+        res_img = put_text_box(bbox, cls_name, res_img)
+        images.append(res_img)
+    final_image = concat_images(images)
+    img_name = split_extension(os.path.split(args.img_path)[-1], suffix='-res')
+    output_path = f'outputs/{img_name}'
+    os.makedirs("outputs", exist_ok=True)
+    cv2.imwrite(output_path, final_image)
+    show_destroy_cv2(final_image)
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/gradcam.py
@@ -0,0 +1,84 @@
+import time
+import torch
+import torch.nn.functional as F
+
+
+def find_yolo_layer(model, layer_name):
+    """Find yolov5 layer to calculate GradCAM and GradCAM++
+
+    Args:
+        model: yolov5 model.
+        layer_name (str): the name of layer with its hierarchical information.
+
+    Return:
+        target_layer: found layer
+    """
+    hierarchy = layer_name.split('_')
+    target_layer = model.model._modules[hierarchy[0]]
+
+    for h in hierarchy[1:]:
+        target_layer = target_layer._modules[h]
+    return target_layer
+
+
+class YOLOV5GradCAM:
+
+    def __init__(self, model, layer_name, img_size=(640, 640)):
+        self.model = model
+        self.gradients = dict()
+        self.activations = dict()
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients['value'] = grad_output[0]
+            return None
+
+        def forward_hook(module, input, output):
+            self.activations['value'] = output
+            return None
+
+        target_layer = find_yolo_layer(self.model, layer_name)
+        target_layer.register_forward_hook(forward_hook)
+        target_layer.register_backward_hook(backward_hook)
+
+        device = 'cuda' if next(self.model.model.parameters()).is_cuda else 'cpu'
+        self.model(torch.zeros(1, 3, *img_size, device=device))
+        print('[INFO] saliency_map size :', self.activations['value'].shape[2:])
+
+    def forward(self, input_img, class_idx=True):
+        """
+        Args:
+            input_img: input image with shape of (1, 3, H, W)
+        Return:
+            mask: saliency map of the same spatial dimension with input
+            logit: model output
+            preds: The object predictions
+        """
+        saliency_maps = []
+        b, c, h, w = input_img.size()
+        tic = time.time()
+        preds, logits = self.model(input_img)
+        print("[INFO] model-forward took: ", round(time.time() - tic, 4), 'seconds')
+        for logit, cls, cls_name in zip(logits[0], preds[1][0], preds[2][0]):
+            if class_idx:
+                score = logit[cls]
+            else:
+                score = logit.max()
+            self.model.zero_grad()
+            tic = time.time()
+            score.backward(retain_graph=True)
+            print(f"[INFO] {cls_name}, model-backward took: ", round(time.time() - tic, 4), 'seconds')
+            gradients = self.gradients['value']
+            activations = self.activations['value']
+            b, k, u, v = gradients.size()
+            alpha = gradients.view(b, k, -1).mean(2)
+            weights = alpha.view(b, k, 1, 1)
+            saliency_map = (weights * activations).sum(1, keepdim=True)
+            saliency_map = F.relu(saliency_map)
+            saliency_map = F.upsample(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+            saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
+            saliency_map = (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+            saliency_maps.append(saliency_map)
+        return saliency_maps, logits, preds
+
+    def __call__(self, input_img):
+        return self.forward(input_img)

816ea8ff05e4726d25b9e55035c88588addff4a6
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-16 00:50:40 +0430

    Fix folder process issue

--- main.py
+++ main.py
@@ -85,11 +85,40 @@ def main(img_path):
     cv2.imwrite(output_path, final_image)
 
 
+def folder_main(folder_path):
+    device = args.device
+    input_size = (args.img_size, args.img_size)
+    print('[INFO] Loading the model')
+    model = YOLOV5TorchObjectDetector(args.model_path, device, img_size=input_size,
+                                      names=None if args.names is None else args.names.strip().split(","))
+    for item in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, item)
+        img = cv2.imread(img_path)
+        torch_img = model.preprocessing(img[..., ::-1])
+        if args.method == 'gradcam':
+            saliency_method = YOLOV5GradCAM(model=model, layer_name=args.target_layer, img_size=input_size)
+        tic = time.time()
+        masks, logits, [boxes, _, class_names, _] = saliency_method(torch_img)
+        print("total time:", round(time.time() - tic, 4))
+        result = torch_img.squeeze(0).mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).detach().cpu().numpy()
+        result = result[..., ::-1]  # convert to bgr
+        images = [result]
+        for i, mask in enumerate(masks):
+            res_img = result.copy()
+            bbox, cls_name = boxes[0][i], class_names[0][i]
+            res_img, heat_map = get_res_img(bbox, mask, res_img)
+            res_img = put_text_box(bbox, cls_name, res_img)
+            images.append(res_img)
+        final_image = concat_images(images)
+        img_name = split_extension(os.path.split(img_path)[-1], suffix='-res')
+        output_path = f'{args.output_dir}/{img_name}'
+        os.makedirs(args.output_dir, exist_ok=True)
+        print(f'[INFO] Saving the final image at {output_path}')
+        cv2.imwrite(output_path, final_image)
+
+
 if __name__ == '__main__':
     if os.path.isdir(args.img_path):
-        img_list = os.listdir(args.img_path)
-        print(img_list)
-        for item in img_list:
-            main(os.path.join( args.img_path ,item))
+        folder_main(args.img_path)
     else:
         main(args.img_path)
